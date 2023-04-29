cc6b3147be58440f28bedbf65b18ead64bb0f2cd
Kishor Bhosale <kishorevbhosale@gmail.com>
2023-04-29 10:19:51 +0530

    String basic

---
+++ src/main/scala/String.scala
@@ -0,0 +1,9 @@
+@main
+def string(): Unit = {
+  println("cat")
+  println("This is also a string 123 %^&")
+  println("''")
+  println("The answer is \"yes\"")
+  println("This is backslash \\")
+  println("This is new line \n on new line \t space \t for \t every word ")
+}
